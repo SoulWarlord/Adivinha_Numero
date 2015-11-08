fa4d9c510f56f62dfe6982bb5904fec42d6631de
SoulWarlord <andrelascas@hotmail.com>
2015-11-08 15:55:39 +0000

    adicionados comentários extra e dados de autor

--- adivinha_numero.py
+++ adivinha_numero.py
@@ -1,13 +1,20 @@
-#adivinha.py
 #programa para um jogo simples: adivinha o numero
+"""
+_author_: André Lascas
+_version_:1.0.0
+_source_: VASCONCELOS, José Braga de; RIBEIRO, Nuno - Título: Tecnologias de Programação de Jogos,
+FCA Editora
+"""
 
 import random
+
 tentativa = 0
 adivinha = 0
 nome = input("Qual o teu nome jogador? ")
 num = random.randint(1,20)
 print(nome+ ', adivinha o número que estou a pensar entre 1 e 20')
 
+#loop para jogar
 while (tentativa < 5) and (adivinha != num):
     adivinha = input('Adivinha: ')
     adivinha = int(adivinha)
